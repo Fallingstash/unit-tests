18b92a18b962cad0723f69045d7384d23bda6201
Fallingstash <sashkaprog228@gmail.com>
2025-05-11 15:15:26 +0700

    create unit-tests

---
+++ SquareMatrixExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+public class SquareMatrixExtensionsTests {
+  [Fact]
+  public void Transpose_Matrix_CorrectlyTransposed() {
+    // Arrange
+    var matrix = new SquareMatrixExtensions(new int[,] { { 1, 2 }, { 3, 4 } });
+    var expected = new int[,] { { 1, 3 }, { 2, 4 } };
+
+    // Act
+    matrix.TransposeMatrix(matrix);
+
+    // Assert
+    Assert.Equal(expected, matrix.matrix);
+  }
+
+  [Fact]
+  public void Track_Matrix_ReturnsSumOfDiagonal() {
+    // Arrange
+    var matrix = new SquareMatrixExtensions(new int[,] { { 1, 2 }, { 3, 4 } });
+
+    // Act
+    var track = matrix.Track(matrix);
+
+    // Assert
+    Assert.Equal(5, track);
+  }
+}
---
+++ SquareMatrixTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+
+public class SquareMatrixTests {
+  private readonly SquareMatrix _matrix2x2;
+  private readonly SquareMatrix _anotherMatrix2x2;
+  private readonly SquareMatrix _matrix3x3;
+
+  public SquareMatrixTests() {
+    var rand = new Random(42);
+    _matrix2x2 = new SquareMatrix(2, rand);
+    _anotherMatrix2x2 = new SquareMatrix(2, rand);
+    _matrix3x3 = new SquareMatrix(3, rand);
+  }
+
+  [Fact]
+  public void Add_TwoSameSizeMatrices_ReturnsCorrectMatrix() {
+    var expected = new int[,] { { _matrix2x2.matrix[0, 0] + _anotherMatrix2x2.matrix[0, 0], _matrix2x2.matrix[0, 1] + _anotherMatrix2x2.matrix[0, 1] },
+                                 { _matrix2x2.matrix[1, 0] + _anotherMatrix2x2.matrix[1, 0] , _matrix2x2.matrix[1, 1] + _anotherMatrix2x2.matrix[1, 1] } };
+
+    var result = _matrix2x2 + _anotherMatrix2x2;
+
+    Assert.Equal(expected, result.matrix);
+  }
+
+  [Fact]
+  public void Multiply_Matrix() {
+
+    var expected = new int[,] { { _matrix2x2.matrix[0, 0] * _anotherMatrix2x2.matrix[0, 0] + _matrix2x2.matrix[0, 1] * _anotherMatrix2x2.matrix[1, 0], _matrix2x2.matrix[0, 0] * _anotherMatrix2x2.matrix[0, 1] + _matrix2x2.matrix[0, 1] * _anotherMatrix2x2.matrix[1, 1] },
+      { _matrix2x2.matrix[1, 0] * _anotherMatrix2x2.matrix[0, 0] + _matrix2x2.matrix[1, 1] * _anotherMatrix2x2.matrix[1, 0], _matrix2x2.matrix[1, 0] * _anotherMatrix2x2.matrix[0, 1] + _matrix2x2.matrix[1, 1] * _anotherMatrix2x2.matrix[1, 1] } };
+
+    var result = _matrix2x2 * _anotherMatrix2x2;
+
+    Assert.Equal(expected, result.matrix);
+  }
+
+  [Fact]
+  public void Compare_Matrices_ReturnsCorrectResult() {
+    var matrixA = new SquareMatrix(new int[,] { { 1, 2 }, { 3, 4 } });
+    var matrixB = new SquareMatrix(new int[,] { { 1, 2 }, { 3, 4 } });
+
+    Assert.True(matrixA == matrixB);
+  }
+
+  [Fact]
+  public void Determinant_2x2Matrix_ReturnsCorrectValue() {
+    var matrix = new SquareMatrix(new int[,] { { 1, 2 }, { 3, 4 } });
+
+    var det = matrix.Determinant();
+
+    Assert.Equal(-2, det);
+  }
+}
